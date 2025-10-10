c7de491dee864a78855b90604efe380dacaa36d6
Davve420 <davidbrolin04@gmail.com>
2025-10-10 10:43:49 +0200

    Ny backgrund och styling på cards och form

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitfunction(){
+        alert("Thanks you for submitting, Hornet will be in touch");
+    }
+
+function notEnoughRosaries() {
+        alert("You don't have enough Rosaries");
+        }
